1b50430cac0726c70c4092e1f71c88dd387587a2
vigneshwaranmuniyandi <vigneshwaran.muniyandi@kaaylabs.com>
2022-12-29 22:42:54 +0530

    project basic structure

---
+++ ecommerce-services/src/main/java/com/kl/ecommerce/EcommerceServicesApplication.java
@@ -0,0 +1,13 @@
+package com.kl.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServicesApplication.class, args);
+	}
+
+}
